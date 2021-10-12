040baa9a17cc5b971d865fd71bbde192a7d1458a
cahitYes <efeyesiloglu@hotmail.com>
2021-10-12 16:14:29 +0200

    switch get pg

--- index.php
+++ index.php
@@ -3,4 +3,21 @@
 /*
 controleur frontal
 */
+// chargement des dépendances
+require_once "config.php";
 
+// routeur
+if(!isset($_GET[`pages`])){
+    // chargement de l'accueil
+}
+else{
+    //on est pas sur l'accueil
+    switch($_GET[`pages`]){
+        case "cv":
+            // import du cv
+            break;
+        case "tuto":
+            
+            break;
+    }
+}
